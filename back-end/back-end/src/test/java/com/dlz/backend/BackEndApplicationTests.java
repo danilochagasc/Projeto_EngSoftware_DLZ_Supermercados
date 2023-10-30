7868824a4ee9064c21474efbe6fdad106bb790b4
Lara Linhares <lararlinhares@gmail.com>
2023-10-30 19:04:29 -0300

    Pasta back-end

---
+++ back-end/back-end/src/test/java/com/dlz/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
