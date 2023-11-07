2f5e94a7c902a5940875e6ca2c3912f2379831d3
joze08 <63240949+joze08@users.noreply.github.com>
2023-11-07 17:15:01 -0300

    Criando tela de login

---
+++ front-end/src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ButtonStyle } from "./style";
+
+//Propertie size has the values: large, medium, small.
+export default function Button(props) {
+  let size;
+
+  if (props.size === "large") {
+    size = { width: "16em", height: "3em" };
+  } else {
+    size = { width: "3em", height: "1em" };
+  }
+
+  return (
+    <ButtonStyle style={size}>{props.children}</ButtonStyle>
+  );
+}
+
+
