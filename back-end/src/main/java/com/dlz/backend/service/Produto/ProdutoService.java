cc4cff1fd569d7cdbd4d8772d0da985cf132a03f
Danilo Chagas <75141085+danilochagasc@users.noreply.github.com>
2023-11-07 12:18:35 -0300

    Atualizacao do ProdutoService

--- back-end/src/main/java/com/dlz/backend/service/Produto/ProdutoService.java
+++ back-end/src/main/java/com/dlz/backend/service/Produto/ProdutoService.java
@@ -1,5 +1,22 @@
 package com.dlz.backend.service.Produto;
 
+import com.dlz.backend.dto.request.ProdutoRequestDTO;
+import com.dlz.backend.dto.response.ProdutoResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ProdutoService {
 
+    ProdutoResponseDTO encontrarPorId(UUID id);
+
+    List<ProdutoResponseDTO> listarTodos();
+
+    List<ProdutoResponseDTO> listarPorDepartamento(String nomeDep);
+
+    ProdutoResponseDTO registrar(ProdutoRequestDTO produtoRequestDTO);
+
+    ProdutoResponseDTO atualizar(UUID id, ProdutoRequestDTO produtoRequestDTO);
+
+    String deletar(UUID id);
 }
