31f582fa9ecccbef88880716f4e6e0ab15e0d080
joze08 <63240949+joze08@users.noreply.github.com>
2023-11-08 13:28:44 -0300

    Criando tela de login

---
+++ front-end/src/components/Button/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonStyle = styled.button`
+  align-self: center;
+  background-color: #3A62C8;
+  
+  border: none;
+  border-radius: 30px;
+  height: 50px;
+
+  font-size: 120%;
+  font-weight: bold;
+  color: white;
+`;
+
+
+
---
+++ front-end/src/pages/Login/Login.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { InputField, Button } from '../../components';
+import logoDLZ from '../../assets/logoDLZ.svg';
+import facebook from '../../assets/facebook.svg';
+import google from '../../assets/google.png';
+import "./style.css";
+
+export default function Login() {
+  return (
+    <main className="main">
+      <div className="background"></div>
+      <aside className="left-aside">
+        <img src={logoDLZ} alt="Logo" className="logo" />
+      </aside>
+
+      <div className="right-aside">
+        <h1 className="title">Login</h1>
+        <div className="line-title"></div>
+        <form action="" className="form">
+          <InputField placeholder="Email" type="email" />
+          <InputField placeholder="Senha" type="password" />
+
+          <a href="#" className="forgot-password">Esqueceu sua senha?</a>
+
+          <Button size="large">Login</Button>
+        </form>
+
+        <div className="or">
+          <div className="line-or"></div>
+          <p>ou</p>
+          <div className="line-or"></div>
+        </div>
+
+        <div className="logins">
+          <div className="facebook-login"><img src={facebook} alt="" className="login-image" /></div>
+          <div className="google-login"><img src={google} alt="" className="login-image" /></div>
+        </div>
+
+        <p className="signin">Não tem uma conta? <a href="#" className="signin-link">Registre-se</a></p>
+      </div>
+    </main>
+  );
+}
