240c7b4f6295cf325fabf33eb890851da62a4cbf
Danilo Chagas <75141085+danilochagasc@users.noreply.github.com>
2023-11-12 18:25:12 -0300

    Criados DTOs de Compra

---
+++ back-end/src/main/java/com/dlz/backend/dto/request/CompraRequestDTO.java
@@ -0,0 +1,13 @@
+package com.dlz.backend.dto.request;
+
+import com.dlz.backend.model.Carrinho;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class CompraRequestDTO {
+
+    private int valorTotal;
+
+}
