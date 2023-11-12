97545a6cc998b092f9b60312d65b6998b59e3063
Danilo Chagas <75141085+danilochagasc@users.noreply.github.com>
2023-11-12 18:24:14 -0300

    Correções nos Repositories Carrinho e Compra

--- back-end/src/main/java/com/dlz/backend/repository/CarrinhoRepository.java
+++ back-end/src/main/java/com/dlz/backend/repository/CarrinhoRepository.java
@@ -2,10 +2,15 @@ package com.dlz.backend.repository;
 
 import com.dlz.backend.model.Carrinho;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface CarrinhoRepository extends JpaRepository<Carrinho, UUID> {
+
+    //Query que retorna o carrinho pelo id do cliente
+    @Query("SELECT c FROM Carrinho c WHERE c.cliente.idCliente = ?1")
+    Carrinho findByIdCliente(UUID idCliente);
 }
