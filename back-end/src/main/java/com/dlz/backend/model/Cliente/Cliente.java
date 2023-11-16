9b7b315c735766602af663d490bfb699e0e4feec
Danilo Chagas <75141085+danilochagasc@users.noreply.github.com>
2023-11-16 12:00:39 -0300

    Adicionados Metodos de Autenticacao

--- back-end/src/main/java/com/dlz/backend/model/Cliente/Cliente.java
+++ back-end/src/main/java/com/dlz/backend/model/Cliente/Cliente.java
@@ -3,7 +3,12 @@ package com.dlz.backend.model.Cliente;
 import com.dlz.backend.model.Carrinho;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -11,7 +16,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Cliente {
+public class Cliente implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -50,5 +55,42 @@ public class Cliente {
         this.endereco = endereco;
         this.permissao = permissao;
         this.carrinho = carrinho;
+    }
+
+    //metodos do UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(getPermissao() == ClientePermissao.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
