15cac865597601ee472996a73c42e00ed6a264aa
Danilo Chagas <danilovga1000@gmail.com>
2024-07-23 12:51:30 -0300

    iniciado projeto back

---
+++ back-end/src/test/java/dlz/com/br/back_end/StartupTests.java
@@ -0,0 +1,13 @@
+package dlz.com.br.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
