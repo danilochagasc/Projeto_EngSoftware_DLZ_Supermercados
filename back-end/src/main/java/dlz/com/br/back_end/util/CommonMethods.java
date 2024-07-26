1a621cbbb7be6816afb6964faafd5eaa3bda11a6
danilochagasc <danilovga1000@gmail.com>
2024-07-26 09:12:02 -0300

    criada classe utilitária

--- back-end/src/main/java/dlz/com/br/back_end/util/CommonMethods.java
+++ back-end/src/main/java/dlz/com/br/back_end/util/CommonMethods.java
@@ -1,10 +1,11 @@
 package dlz.com.br.back_end.util;
 
+import dlz.com.br.back_end.exception.general.EntityNotFound;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class CommonMethods {
 
     public static <T> T getEntityById(Long id, JpaRepository<T, Long> repository) {
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("Entity not found"));
+        return repository.findById(id).orElseThrow(() -> new EntityNotFound("Entity not found with id: " + id));
     }
 }
