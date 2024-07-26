34edc5cd67adfe3c12da8914500a6b604d95a950
danilochagasc <danilovga1000@gmail.com>
2024-07-26 09:14:29 -0300

    classe utilitaria

--- back-end/src/main/java/dlz/com/br/back_end/util/MediaType.java
+++ back-end/src/main/java/dlz/com/br/back_end/util/MediaType.java
@@ -3,4 +3,6 @@ package dlz.com.br.back_end.util;
 public class MediaType {
 
     public static final String APPLICATION_JSON = "application/json";
+
+    public static final String PLAIN_TEXT = "text/plain";
 }
