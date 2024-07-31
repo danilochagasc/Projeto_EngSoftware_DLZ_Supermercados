b6bc1fce7bc5ebdd864abdbc9e34dc1ac8ed49fe
danilochagasc <danilovga1000@gmail.com>
2024-07-31 17:07:55 -0300

    criada tela login

--- front-end/src/app/pages/login/login.component.ts
+++ front-end/src/app/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormInputBarComponent } from '../../components/form-input-bar/form-input-bar.component';
+import { LoginRegisterButtonComponent } from "../../components/login-register-button/login-register-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormInputBarComponent, LoginRegisterButtonComponent, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
